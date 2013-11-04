1a8f676605527634279b63f242a2813cdcdd8b2d
Gabrielle Bennett <gmb9280@rit.edu>
2013-11-04 13:12:55 -0500

    Added Project
    It's set up so we can delete the SQL database and the _DELETE folder and
    it will be smaller.  Now to add OpenGL -- this will be fun.

---
+++ Project/Solution/Main.cpp
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------------------
+This project was made by Gabrielle Bennett and Amanda Rivet in 2013.
+All rights reserved. 
+--------------------------------------------------------------------------------------------------*/
+#include "Main.h"
+
+int main()
+{
+	std::cout << "Hello World\n";
+
+	//Ending the program
+	std::cout << "Press enter to finish";
+	getchar();
+	return 0;
+}
---
+++ Project/Solution/Main.h
@@ -0,0 +1,10 @@
+/*--------------------------------------------------------------------------------------------------
+This project was made by Gabrielle Bennett and Amanda Rivet in 2013. 
+All rights reserved. 
+--------------------------------------------------------------------------------------------------*/
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+#endif //_MAIN_H
